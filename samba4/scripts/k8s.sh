b4ea1df28f9fcc4631b163c1a12ce206fa1ca5a4
James Greenhill <fuziontech@gmail.com>
2020-04-13 20:53:39 -0700

    probably bad changes to make my setup work

--- samba4/scripts/k8s.sh
+++ samba4/scripts/k8s.sh
@@ -4,18 +4,18 @@ if [ ! -f /privatepersist/etc/passwd ]; then
   mkdir -p /privatepersist/etc
   cp /etc/passwd /privatepersist/etc/passwd
 fi
-rm -f /etc/passwd
-ln -s /privatepersist/etc/passwd /etc/passwd
+# rm -f /etc/passwd
+# ln -s /privatepersist/etc/passwd /etc/passwd
 #tdb files need to be stored on the persistent storage
 if [ ! -f /privatepersist/varlibsamba/private/passdb.tdb ]; then
   mkdir /privatepersist/varlibsamba
   tar c -C /var/lib/samba . | tar x -C /privatepersist/varlibsamba
 fi
-rm -rf /var/lib/samba
-ln -s /privatepersist/varlibsamba /var/lib/samba
+# rm -rf /var/lib/samba
+# ln -s /privatepersist/varlibsamba /var/lib/samba
 #create required users
 if ! md5sum -c /privatepersist/k8s-users-checksum > /dev/null 2>&1; then
-  rm /var/lib/samba/private/passdb.tdb
+  # rm /var/lib/samba/private/passdb.tdb
   if [ -s /secrets/create-users ]; then
     for line in $(cat /secrets/create-users); do
       USER=$(echo $line | cut -f1 -d:)
